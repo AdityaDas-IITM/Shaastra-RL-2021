027b60128154dac4a16a289b875b439a69d339b7
Aditya Das <adas2105.ad@gmail.com>
2021-02-18 09:52:33 +0530

    Vizdoom fixed

--- DQN/Vizdoom/train.py
+++ DQN/Vizdoom/train.py
@@ -20,14 +20,12 @@ def train(game, episodes, eps, eps_decay, agent, input_shape, max_rew = -1e5, re
         while not game.is_episode_finished():
             state = game.get_state().screen_buffer
             state= cv2.resize(state, (input_shape, input_shape))
-            health = game.get_game_variable(HEALTH)
             if len(state.shape)==2:
                 state = np.expand_dims(state, axis=0)
             
-            action = agent.get_action(state, health, eps)
+            action = agent.get_action(state, eps)
             reward = game.make_action(action)
 
-            
             done = 1 if game.is_episode_finished() else 0
             if not done:
                 next_state = game.get_state().screen_buffer
@@ -37,10 +35,8 @@ def train(game, episodes, eps, eps_decay, agent, input_shape, max_rew = -1e5, re
             else:
                 next_state = np.zeros((input_channels, input_shape, input_shape))
 
-            next_health = game.get_game_variable(HEALTH)
-            agent.step(state, health, action, reward, next_state, next_health, done)
+            agent.step(state, action, reward, next_state, done)
             state = next_state
-            health = next_health
             eps*=eps_decay
             total_rew+=reward
             #time.sleep(0.02)
@@ -48,7 +44,7 @@ def train(game, episodes, eps, eps_decay, agent, input_shape, max_rew = -1e5, re
         print (f'Episode {i} Reward {total_rew}')
         if total_rew >= max_rew:
             max_rew = total_rew
-            torch.save(agent.qnetwork_local.state_dict(), f'./checkpoints/defend_the_line{total_rew}.pth')
+            torch.save(agent.qnetwork_local.state_dict(), f'./checkpoints/basic{total_rew}.pth')
 
 def infer(game, agent, episodes, arbit = False, path = ''):
     game.set_window_visible(True)
@@ -60,12 +56,11 @@ def infer(game, agent, episodes, arbit = False, path = ''):
         
         while not game.is_episode_finished():
             state = game.get_state().screen_buffer
-            health = game.get_game_variable(HEALTH)
             state= cv2.resize(state, (input_shape, input_shape))
             if len(state.shape)==2:
                 state = np.expand_dims(state, axis=0)
             if not arbit:
-                action = agent.get_action(state, health, eps=0)
+                action = agent.get_action(state, eps=0)
             else:
                 action_idx = random.choice(range(action_size))
                 action = [0]*action_size
@@ -76,7 +71,7 @@ def infer(game, agent, episodes, arbit = False, path = ''):
 
 if __name__ == '__main__':
     game = DoomGame()
-    game.load_config("./scenarios/defend_the_line.cfg")
+    game.load_config("./scenarios/basic.cfg")
     game.set_screen_format(ScreenFormat.GRAY8)
 
     input_shape = 50
@@ -88,5 +83,5 @@ if __name__ == '__main__':
     max_rew = -1e5
     episodes = 5000
 
-    train(game, episodes, eps, eps_decay, agent, input_shape)
-    #infer(game, agent, episodes = 5, arbit = True, path = './checkpoints/defend_the_line30.0.pth')
+    #train(game, episodes, eps, eps_decay, agent, input_shape)
+    infer(game, agent, episodes = 5, arbit = False, path = './checkpoints/basic.pth')
